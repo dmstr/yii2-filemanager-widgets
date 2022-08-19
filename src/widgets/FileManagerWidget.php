b9aaf197826d877cd61270e44a8f7e717401a766
Jens Giessmann <j.giessmann@herzogkommunikation.de>
2022-08-19 14:55:31 +0200

    make angular app configurable via widget options property
    - add new widget property options which can be used to set all(?) angularFileMangerApp properties
    - define (sane) default for the angularFileMangerApp
    - cleanup some unused widget properties

--- src/widgets/FileManagerWidget.php
+++ src/widgets/FileManagerWidget.php
@@ -11,6 +11,8 @@ namespace hrzg\filemanager\widgets;
 
 use hrzg\filemanager\assets\AfmAsset;
 use yii\base\Widget;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
 use yii\helpers\Url;
 use yii\web\View;
 
@@ -40,24 +42,22 @@ class FileManagerWidget extends Widget
     public $template = '<div data-ng-app="FileManagerApp"><div class="ng-cloak"><angular-filemanager></angular-filemanager></div></div>';
 
     /**
-     * @var string
-     */
-    public $thumbnailUrlPrefix;
-
-    /**
-     * @var string
-     */
-    public $thumbnailUrlSuffix;
-
-    /**
-     * @var boolean
+     * @var boolean|null
      */
     public $enableThumbnails;
 
     /**
-     * @var boolean
+     * @var boolean|null
      */
     public $enableIconPreviewView;
+
+    /**
+     * array of angular fileManagerApp options
+     *
+     * @var null|array
+     */
+    public $options;
+
     /**
      * @inheritdoc
      */
@@ -97,74 +97,103 @@ class FileManagerWidget extends Widget
         $title             = empty($this->title) ? getenv('AFM_TITLE') : $this->title;
         $lang              = \Yii::$app->language;
 
-        // resolve options for JavaScript
-        $enableThumbnails = $this->enableThumbnails ? 'true' : 'false';
-        $enableIconPreviewView = $this->enableIconPreviewView ? 'true' : 'false';
+        $this->options = is_array($this->options) ? $this->options : [];
 
+        $defaults = [
+            'appName' => $title,
+            'defaultLang' => $lang,
+            // which functions should be activated in fileManagerApp?
+            'searchForm' => true,
+            'sidebar' => true,
+            'breadcrumb' => true,
+            'hidePermissions' => true,
+
+            // Handler Urls
+            'listUrl' => $this->handlerUrl,
+            'uploadUrl' => $this->handlerUrl,
+            'renameUrl' => $this->handlerUrl,
+            'copyUrl' => $this->handlerUrl,
+            'moveUrl' => $this->handlerUrl,
+            'removeUrl' => $this->handlerUrl,
+            'getContentUrl' => $this->handlerUrl,
+            'createFolderUrl' => $this->handlerUrl,
+            'downloadFileUrl' => $this->handlerUrl,
+            'downloadMultipleUrl' => $this->handlerUrl,
+            'compressUrl' => $this->handlerUrl,
+            'extractUrl' => $this->handlerUrl,
+            'permissionsUrl' => $this->handlerUrl,
+
+            // Additional settings
+            'multipleDownloadFileName' => 'filemanager.zip',
+            'showSizeForDirectories' => false,
+            'useBinarySizePrefixes' => false,
+            'downloadFilesByAjax' => true,
+            'previewImagesInModal' => true,
+            'enablePermissionsRecursive' => false,
+            'enableThumbnails' => false,
+            'enableIconPreviewView' => false,
+
+            // File patterns
+            'isEditableFilePattern' => '/\.(!)/i',
+            'isImageFilePattern' => '/\.(jpe?g|gif|bmp|png|svg|tiff?)$/i',
+            'isExtractableFilePattern' => '/\.(gz|tar|rar|g?zip)$/i',
+            // define allowed actions for filemanager
+            'allowedActions' => [
+                'upload' => true,
+                'rename' => false,
+                'move' => true,
+                'copy' => true,
+                'edit' => true,
+                'compress' => true,
+                'compressChooseName' => true,
+                'extract' => true,
+                'download' => true,
+                'downloadMultiple' => true,
+                'downloadLink' => true,
+                'preview' => true,
+                'remove' => true,
+                'createFolder' => true,
+                'pickFiles' => false,
+                'pickFolders' => false,
+                'changePermissions' => $allowPermissions,
+            ]
+        ];
+
+        // these options are regEx and therefor can not be stored as strings in json
+        // if preset, they will be handled later, see search/replace
+        $specialOpts = [
+            'isEditableFilePattern',
+            'isImageFilePattern',
+            'isExtractableFilePattern'
+        ];
+        // store specialOpts for replacement
+        $specialValues = [];
+
+        $config = ArrayHelper::merge($defaults, $this->options);
+
+        // now insert placeholders for $specialValues that can not be stored as strings, e.g. regexP
+        foreach ($specialOpts as $opt) {
+            if (!empty($config[$opt])) {
+                $specialValues[$opt] = [
+                    'value' => $config[$opt],
+                    'placeholder' => '###' . strtoupper($opt) . '###',
+                ];
+                $config[$opt] = $specialValues[$opt]['placeholder'];
+            }
+        }
+        // generate config json
+        $configJson = json_encode($config);
+        // then replace placeholder strings in json config that can/should not be strings...
+        foreach ($specialValues as $special) {
+            $configJson = str_replace('"' . $special['placeholder']. '"', $special['value'], $configJson);
+        }
+
+        // finally create JS to init fileManagerApp
         $initFilemanagerJs = <<<JS
 angular.module('FileManagerApp').config(['fileManagerConfigProvider', function (config) {
     var defaults = config.\$get();
-    var handler = '$this->handlerUrl';
-    config.set({
-
-        // Application
-        appName: '$title',
-        defaultLang: '$lang',
-        searchForm: true,
-        sidebar: true,
-        breadcrumb: true,
-        hidePermissions: true,
-
-        // Allowed actions
-        allowedActions: angular.extend(defaults.allowedActions, {
-            remove: true,
-            list: true,
-            move: true,
-            rename: true,
-            copy: true,
-            download: true,
-            downloadMultiple: false,
-            downloadLink: true,
-            changePermissions: '$allowPermissions',
-            compress: false,
-            compressChooseName: false,
-            extract: false,
-            upload: true
-        }),
-
-        // Handler
-        listUrl: handler,
-        uploadUrl: handler,
-        renameUrl: handler,
-        copyUrl: handler,
-        moveUrl: handler,
-        removeUrl: handler,
-        getContentUrl: handler,
-        createFolderUrl: handler,
-        downloadFileUrl: handler,
-        downloadMultipleUrl: handler,
-        compressUrl: handler,
-        extractUrl: handler,
-        permissionsUrl: handler,
-
-        // Additional settings
-        multipleDownloadFileName: 'filemanager.zip',
-        showSizeForDirectories: false,
-        useBinarySizePrefixes: false,
-        downloadFilesByAjax: true,
-        previewImagesInModal: true,
-        enablePermissionsRecursive: false,
-        thumbnailUrlPrefix: '{$this->thumbnailUrlPrefix}',
-        thumbnailUrlSuffix: '{$this->thumbnailUrlSuffix}',
-        enableThumbnails: {$enableThumbnails},
-        enableIconPreviewView: {$enableIconPreviewView},
-
-        // File patterns
-        isEditableFilePattern: /\.(!)/i,
-        isImageFilePattern: /\.(jpe?g|gif|bmp|png|svg|tiff?)$/i,
-        isExtractableFilePattern: /\.(gz|tar|rar|g?zip)$/i
-        //tplPath: 'src/templates'
-    });
+    // we use merge here to get deep.copy, see: https://docs.angularjs.org/api/ng/function/angular.merge
+    config.set(angular.merge(defaults, $configJson));
 }]);
 JS;
 
